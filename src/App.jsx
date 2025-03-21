fde566d31e2f82ef89091a648472bfbfc01acb74
Varun <varunshaji44@gmail.com>
2025-03-21 10:45:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SubmitButton from "./Components/Button/SubmitButton"
+
+function App() 
+{
+ return(
+  <header>
+    <h1>Hello World</h1>
+    < SubmitButton />
+  </header>
+ )
+}
+
+export default App
---
+++ src/Components/Button/SubmitButton.stories.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import SubmitButton from './SubmitButton';
+
+// Storybook metadata
+export default {
+  title: 'Components/SubmitButton',
+  component: SubmitButton,
+};
+
+// Default story
+export const Default = () => <SubmitButton />;
