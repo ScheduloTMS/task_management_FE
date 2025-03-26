4d4cb30a853ad47def6d15a3a42352f77e15d1c0
AnnaEliswa <annaeliswa@gmail.com>
2025-03-26 18:14:26 +0530

    Sidebar Added

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { MdTask } from "react-icons/md";
+import { AiFillMessage } from "react-icons/ai";
+import { GoHomeFill } from "react-icons/go";
+import { TbLayoutSidebarLeftCollapse ,TbLayoutSidebarRightCollapse  } from "react-icons/tb";
+import { RiCalendarScheduleFill } from "react-icons/ri";
+import "./Sidebar.css";
+import Logo from "../logo/Logo";
+
+
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const navigate = (path) => {
+    window.location.href = path;
+  };
+
+  return (
+    <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+      <div className="sidebar-header">
+      <Logo />
+        <button className="collapse-btn" onClick={() => setCollapsed(!collapsed)}>
+          {collapsed ? <TbLayoutSidebarRightCollapse /> : <TbLayoutSidebarLeftCollapse />}
+        </button>
+      </div>
+      <nav className="sidebar-menu">
+        <div className="sidebar-item" onClick={() => navigate("/")}>
+          <GoHomeFill /> <span className="sidebar-text">Home</span>
+        </div>
+        <div className="sidebar-item" onClick={() => navigate("/tasks")}>
+          <MdTask  /> <span className="sidebar-text">My Tasks</span>
+        </div>
+        <div className="sidebar-item" onClick={() => navigate("/messages")}>
+          <AiFillMessage   /> <span className="sidebar-text">Messages</span>
+        </div>
+        <div className="sidebar-item" onClick={() => navigate("/calender")}>
+        <RiCalendarScheduleFill /> <span className="sidebar-text">Calendar</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/layouts/modal-layout/ModalLayout.jsx
+++ src/layouts/modal-layout/ModalLayout.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import Alert from "../components/modal/Alert";
+import Alert from "../../components/modal/Alert";
 
 export const SuccessAlert = (props) => (
   <Alert 
