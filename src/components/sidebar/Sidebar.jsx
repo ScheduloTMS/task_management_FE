9909661146e4215c367618f6680898de0b694f36
jyothirmayiks <jothirmayiks@gmail.com>
2025-04-05 19:36:02 +0530

    refactor: remove sidebar collapse

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import { MdTask } from "react-icons/md";
 import { AiFillMessage } from "react-icons/ai";
 import { GoHomeFill } from "react-icons/go";
-import { TbLayoutSidebarLeftCollapse ,TbLayoutSidebarRightCollapse  } from "react-icons/tb";
 import { RiCalendarScheduleFill } from "react-icons/ri";
+import { IoPeople } from "react-icons/io5";
 import "./Sidebar.css";
 import Logo from "../logo/Logo";
 
-
-const Sidebar = () => {
-  const [collapsed, setCollapsed] = useState(false);
-
+const Sidebar = ({ role }) => {
   const navigate = (path) => {
     window.location.href = path;
   };
 
   return (
-    <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+    <div className="sidebar">
       <div className="sidebar-header">
-      <Logo size="small"/>
-        <button className="collapse-btn" onClick={() => setCollapsed(!collapsed)}>
-          {collapsed ? <TbLayoutSidebarRightCollapse /> : <TbLayoutSidebarLeftCollapse />}
-        </button>
+        <Logo size="small" />
       </div>
+
       <nav className="sidebar-menu">
         <div className="sidebar-item" onClick={() => navigate("/")}>
           <GoHomeFill /> <span className="sidebar-text">Home</span>
         </div>
+
         <div className="sidebar-item" onClick={() => navigate("/tasks")}>
-          <MdTask  /> <span className="sidebar-text">My Tasks</span>
+          <MdTask /> <span className="sidebar-text">My Tasks</span>
         </div>
+
+        {role === "MENTOR" && (
+          <div className="sidebar-item" onClick={() => navigate("/team")}>
+            <IoPeople /> <span className="sidebar-text">Team</span>
+          </div>
+        )}
+
         <div className="sidebar-item" onClick={() => navigate("/messages")}>
-          <AiFillMessage   /> <span className="sidebar-text">Messages</span>
+          <AiFillMessage /> <span className="sidebar-text">Messages</span>
         </div>
-        <div className="sidebar-item" onClick={() => navigate("/calender")}>
-        <RiCalendarScheduleFill /> <span className="sidebar-text">Calendar</span>
+
+        <div className="sidebar-item" onClick={() => navigate("/calendar")}>
+          <RiCalendarScheduleFill /> <span className="sidebar-text">Calendar</span>
         </div>
       </nav>
     </div>
